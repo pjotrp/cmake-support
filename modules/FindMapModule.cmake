dd34ec93b471efd59b314b452cd53cbb6d77f31c
Pjotr Prins <pjotr.public01@thebird.nl>
2009-06-04 15:34:56 +0200

    Introducing a special library for R mapping Allow disabling the search for a module library

--- modules/FindMapModule.cmake
+++ modules/FindMapModule.cmake
@@ -6,6 +6,7 @@
 #   M_NAME               - module name
 #   M_VERSION            - module version (optional)
 #   USE_INCLUDEPATH      - override default location (relative path)
+#   SKIP_MODULE_LIBRARY_SEARCH  - skip library search (found through another way)
 #
 # to return
 #
@@ -29,39 +30,41 @@ IF(NOT IS_DIRECTORY ${MODULE_SOURCE_PATH})
   MESSAGE(FATAL_ERROR "${MODULE_SOURCE_PATH} does not exist")
 ENDIF(NOT IS_DIRECTORY ${MODULE_SOURCE_PATH})
 
-SET(_LIBNAME ${MAP_projectname}_${M_NAME}-${MAP_VERSION})
-SET(_LINKLIB lib${_LIBNAME}.so)
-IF(CYGWIN)
-  SET(_LINKLIB lib${_LIBNAME}.dll.a)
-  SET(MODULE_LIBRARY_BINPATH ${MODULE_SOURCE_PATH}/build)
-ENDIF(CYGWIN)
-IF(APPLE)
-  SET(_LINKLIB lib${_LIBNAME}.dylib)
-ENDIF(APPLE)
-MESSAGE("Looking for ${_LINKLIB}")
+IF(NOT SKIP_MODULE_LIBRARY_SEARCH)
+  SET(_LIBNAME ${MAP_projectname}_${M_NAME}-${MAP_VERSION})
+  SET(_LINKLIB lib${_LIBNAME}.so)
+  IF(CYGWIN)
+    SET(_LINKLIB lib${_LIBNAME}.dll.a)
+    SET(MODULE_LIBRARY_BINPATH ${MODULE_SOURCE_PATH}/build)
+  ENDIF(CYGWIN)
+  IF(APPLE)
+    SET(_LINKLIB lib${_LIBNAME}.dylib)
+  ENDIF(APPLE)
+  MESSAGE("Looking for ${_LINKLIB}")
 
-IF(BUILD_LIBS)
-  # This section gets run when cmake is invoked from the top level dir
-  SET(MODULE_LIBRARY ${_LIBNAME})
-ELSE()
-  # This section gets run when CMake is invoked from a node - it needs
-  # an explicit path as is has been pre-built and CMake is not aware...
-  MESSAGE("Actively looking for link library ${_LINKLIB} in ${MODULE_SOURCE_PATH}/build")
-  FIND_LIBRARY(MODULE_LIBRARY NAMES ${_LINKLIB} HINTS ${MODULE_SOURCE_PATH}/build ${MODULE_SOURCE_PATH}/src ${MODULE_SOURCE_PATH}/${USE_INCLUDEPATH} ${MODULE_SOURCE_PATH})
-  IF(NOT MODULE_LIBRARY)
-    FIND_LIBRARY(MODULE_LIBRARY NAMES ${_LINKLIB} PATHS ${MODULE_SOURCE_PATH}/build ${MODULE_SOURCE_PATH}/src ${MODULE_SOURCE_PATH}/${USE_INCLUDEPATH} ${MODULE_SOURCE_PATH})
+  IF(BUILD_LIBS)
+    # This section gets run when cmake is invoked from the top level dir
+    SET(MODULE_LIBRARY ${_LIBNAME})
+  ELSE()
+    # This section gets run when CMake is invoked from a node - it needs
+    # an explicit path as is has been pre-built and CMake is not aware...
+    MESSAGE("Actively looking for link library ${_LINKLIB} in ${MODULE_SOURCE_PATH}/build")
+    FIND_LIBRARY(MODULE_LIBRARY NAMES ${_LINKLIB} HINTS ${MODULE_SOURCE_PATH}/build ${MODULE_SOURCE_PATH}/src ${MODULE_SOURCE_PATH}/${USE_INCLUDEPATH} ${MODULE_SOURCE_PATH})
+    IF(NOT MODULE_LIBRARY)
+      FIND_LIBRARY(MODULE_LIBRARY NAMES ${_LINKLIB} PATHS ${MODULE_SOURCE_PATH}/build ${MODULE_SOURCE_PATH}/src ${MODULE_SOURCE_PATH}/${USE_INCLUDEPATH} ${MODULE_SOURCE_PATH})
+    ENDIF()
   ENDIF()
-ENDIF()
-message("MODULE_LIBRARY=${MODULE_LIBRARY}")
-INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH}/include)
-INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH}/src)
-INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH})
-IF(USE_INCLUDEPATH)
-  INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH}/${USE_INCLUDEPATH})
-ENDIF()
+  message("MODULE_LIBRARY=${MODULE_LIBRARY}")
+  INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH}/include)
+  INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH}/src)
+  INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH})
+  IF(USE_INCLUDEPATH)
+    INCLUDE_DIRECTORIES(${MODULE_SOURCE_PATH}/${USE_INCLUDEPATH})
+  ENDIF()
 
-# UNSET(_LIBNAME)
-SET(_LIBNAME 'unknown')
+  # UNSET(_LIBNAME)
+  SET(_LIBNAME 'unknown')
+ENDIF(NOT SKIP_MODULE_LIBRARY_SEARCH)
 
 MARK_AS_ADVANCED(
   MODULE_SOURCE_PATH
--- modules/FindRLibs.cmake
+++ modules/FindRLibs.cmake
@@ -23,7 +23,7 @@ IF(R_EXECUTABLE)
 	SET(R_LIKELY_INCLUDE_PATH ${R_PATH}/lib/R/include)
 ENDIF(R_EXECUTABLE)
 
-# ---- Find R.h
+# ---- Find R.h and the Rlib.so shared library
 SET(R_POSSIBLE_INCLUDE_PATHS
   /usr/share/R/include
 )
@@ -43,7 +43,6 @@ FIND_LIBRARY(RBLAS_LIBRARY NAMES libRblas.so PATHS
   ${R_LIBRARY_PATH}
 )
 
-
 MESSAGE("R_EXECUTABLE=${R_EXECUTABLE}")
 MESSAGE("R_INCLUDE_PATH=${R_INCLUDE_PATH}")
 MESSAGE("R_LIBRARY=${R_LIBRARY}")
@@ -53,8 +52,32 @@ if(NOT EXISTS ${R_LIBRARY})
 	message(FATAL_ERROR "${R_LIBRARY} was not found (has R been built as shared library libR.so?)")
 endif(NOT EXISTS ${R_LIBRARY})
 
+# ---- find the biolib_R Clib module for mapping...
+SET(_RLIBNAME ${MAP_projectname}_R)
+SET(_RLIBPATH ${MAP_CLIBS_PATH}/${_RLIBNAME})
+INCLUDE_DIRECTORIES(${_RLIBPATH}/include)
+if(NOT BUILD_LIBS)
+  SET(_LIBNAME ${_RLIBNAME}-${MAP_VERSION})
+  SET(_LINKLIB lib${_LIBNAME}.so)
+  IF(CYGWIN)
+    SET(_LINKLIB lib${_LIBNAME}.dll.a)
+  ENDIF(CYGWIN)
+  IF(APPLE)
+    SET(_LINKLIB lib${_LIBNAME}.dylib)
+  ENDIF(APPLE)
+  message("Looking for ${_LINKLIB} in ${_RLIBPATH}")
+  FIND_LIBRARY(R_LIBRARY NAMES ${_LINKLIB} HINTS ${_RLIBPATH}/build ${_RLIBPATH}/src)
+  IF(NOT BIOLIB_R_LIBRARY)
+    FIND_LIBRARY(BIOLIB_R_LIBRARY NAMES ${_LINKLIB} PATHS ${_RLIBPATH}/build ${_RLIBPATH}/src)
+  ENDIF()
+  message("Found ${BIOLIB_R_LIBRARY}")
+endif(NOT BUILD_LIBS)
+# UNSET(_LIBNAME)
+SET(_LIBNAME 'unknown')
+
 MARK_AS_ADVANCED(
   R_INCLUDE_PATH
   R_EXECUTABLE
   R_LIBRARY
+  BIOLIB_R_LIBRARY
   )
