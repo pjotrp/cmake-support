b1f110925723264e7c9a8e650055f4da6485dd53
Pjotr Prins <pjotr.public01@thebird.nl>
2010-04-20 18:07:00 +0200

    Parameter for Doxygen XML files

--- modules/FindPerlPod.cmake
+++ modules/FindPerlPod.cmake
@@ -20,7 +20,7 @@ ADD_CUSTOM_TARGET(perldoc
   COMMENT "Generating Perl documentation for ${M_NAME}"
   COMMAND ${CMAKE_COMMAND} -E make_directory build
   COMMAND ${SWIG_EXECUTABLE} ${swig_include_dirs} -o build/${M_NAME}.xml -xml ${INTERFACE}
-  COMMAND ${MAP_ROOT}/tools/swig2doc/bin/swig2doc build/${M_NAME}.xml ${MAP_ROOT}/build/doc/xml/apidoc/biolib__affyio_8h.xml  ${MAP_ROOT}/build/doc/xml/apidoc/biolib__affyio_8c.xml
+  COMMAND ${MAP_ROOT}/tools/swig2doc/bin/swig2doc build/${M_NAME}.xml ${DOXY_XML_FILES}
   COMMAND pod2html output/perl/${M_NAME}.pod > build/${M_NAME}.html
   # Copy the files
   COMMAND ${CMAKE_COMMAND} -E make_directory ${MAP_ROOT}/build/pod
